f45a7c66e14e690fc94f95a144877ed772bba963
WizardofHause <jonrhause@gmail.com>
2022-11-08 21:16:27 -0700

    commit tree 25ff9d4084ceb9fd2c6a08ce373f093894217929

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,34 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+# ^^^ when provided a person's name, creates and returns
+# "Hello, my name is _____."
+
+def batch_badge_creator(array)
+    array.map do |s|
+        badge_maker(s)
+    end
+end
+# ^^^ takes an array of names as an argument 
+# and returns an array of badge messages
+
+def assign_rooms(array)
+    array.map.with_index { |item, index|
+        "Hello, #{item}! You'll be assigned to room #{index + 1}!" }
+end
+
+def printer(array)
+    a = batch_badge_creator(array)
+    b = assign_rooms(array)
+    a.each do |s|
+        puts s
+    end
+    b.each do |s|
+        puts s
+    end
+end
+
+binding.pry
+0
